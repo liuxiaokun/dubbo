7421752b145d5d13a068a6edbb16edd96d18c79e
liuxiaokun <liuxiaokun@lvmama.com>
2017-06-05 18:54:49 +0800

    local invoke success

--- lesson_provider/src/main/java/com/fred/lesson/provider/controller/HelloController.java
+++ lesson_provider/src/main/java/com/fred/lesson/provider/controller/HelloController.java
@@ -1,16 +1,22 @@
 package com.fred.lesson.provider.controller;
 
+import com.fred.lesson.api.HelloService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+
 @Controller
 public class HelloController {
+
+    @Resource
+    private HelloService helloService;
 
     @RequestMapping("/hello")
     @ResponseBody
     public String test() {
-        return "hello, world! This com from spring!";
+        return helloService.sayHello("local");
     }
 
 }
--- lesson_provider/src/main/java/com/fred/lesson/provider/service/HelloServiceImpl.java
+++ lesson_provider/src/main/java/com/fred/lesson/provider/service/HelloServiceImpl.java
@@ -1,10 +1,12 @@
 package com.fred.lesson.provider.service;
 
 import com.fred.lesson.api.HelloService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by fred on 6/5/17.
  */
+@Service
 public class HelloServiceImpl implements HelloService {
 
     @Override
